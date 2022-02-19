0fbf9bfc908ec46a24399492ebe95af66e17c268
michal-bogucki <bogucki.michal91@gmail.com>
2022-02-19 11:57:04 +0100

    Add adroid context to LinearLayoutManager

--- app/src/main/java/com/learnig/android/cleanarchitecture/core/di/AppModule.kt
+++ app/src/main/java/com/learnig/android/cleanarchitecture/core/di/AppModule.kt
@@ -4,11 +4,12 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import org.koin.android.ext.koin.androidApplication
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val appModule = module {
     factory {
-        LinearLayoutManager(androidApplication())
+        LinearLayoutManager(androidContext())
     }
 
     factory {
